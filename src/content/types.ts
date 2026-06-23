02a00daa2e90e03dda13eab05e30502361f5c56f
jb <jb@ambivolt.de>
2026-06-23 09:40:47 +0200

    feat: Content-Daten (Typen, Firmendaten, Navigation)

---
+++ src/content/types.ts
@@ -0,0 +1,4 @@
+export type NavItem = { label: string; href: string; children?: NavItem[] };
+export type ProductCardData = {
+  slug: string; title: string; teaser: string; image: string; href: string;
+};
